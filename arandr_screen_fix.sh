3ccf258f16beac0f0d71735dbb69e7a89d131157
jarrodharvey <jarrodharvey@gmail.com>
2020-11-03 18:24:57 +1100

    Added a line that sets caps lock as escape.

--- arandr_screen_fix.sh
+++ arandr_screen_fix.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Set capslock to escape
+setxkbmap -option caps:escape
 xrandr --output VIRTUAL2 --off --output VIRTUAL1 --mode VIRTUAL1.446-1920x1080 --pos 1984x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 sleep 3
 killall wmalauncher 
