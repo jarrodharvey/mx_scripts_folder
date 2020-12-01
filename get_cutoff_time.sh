306edaaf064a74e6255b5dc8f3094a86e66b7c5c
jarrodharvey <jarrodharvey@gmail.com>
2020-12-01 21:35:02 +1100

    Added an energy levels check to the cutoff script

--- get_cutoff_time.sh
+++ get_cutoff_time.sh
@@ -2,6 +2,20 @@
 
 ssh -q root@raspbx.local [[ -f /var/spool/asterisk/outgoing/cutoff.call ]] && echo Cutoff tonight is $( date -d @$( ssh root@raspbx.local stat -c %Y /var/spool/asterisk/outgoing/cutoff.call ) '+%r' ) && exit 0
 
+echo What are your current energy levels?
+echo 1: I am exhausted and could not possibly do more work tonight!
+echo 2: I have less energy than normal.
+echo 3: I feel normal.
+echo 4: I have more energy than normal.
+
+read energy_level
+
+if [ $energy_level = 1 ]
+then
+	echo Whoa! You get the rest of the night off!
+	exit 0
+fi
+
 echo What time is your alarm set for tomorrow morning?
 echo If you do not have an alarm set for tomorrow, type N
 
@@ -9,18 +23,20 @@ read alarm_time
 
 if [ $alarm_time == "N" ]
 then
-	seconds_to_remove=$(($RANDOM * 2 / 3))
-	cutoff=$(date -d "2:00 AM tomorrow -$seconds_to_remove seconds")
+	seconds_to_remove=$(($RANDOM * 2 / $energy_level))
+	cutoff=$(date -d "10:00 PM tomorrow + $energy_level hours -$seconds_to_remove seconds")
 else
-	bedtime=$(date -d "$alarm_time tomorrow -8 hours -30 minutes")	
-	seconds_to_remove=$(expr $RANDOM / 3)	
+	let bedtime_modifier="($energy_level - 2) * 30" 
+	bedtime=$(date -d "$alarm_time tomorrow -9 hours + $bedtime_modifier minutes")	
+	seconds_to_remove=$(expr $RANDOM / $energy_level)	
 	cutoff=$(date -d "$bedtime today -$seconds_to_remove seconds")
 fi
 
 # I will want to do SOME work today...
 if [ $( date -d "$cutoff" '+%s' ) -lt $( date '+%s' ) ]
 then
-	cutoff=$(date -d "today + 1 hour")
+	let minutes_to_work="$energy_level * 25"
+	cutoff=$(date -d "today + $minutes_to_work minutes")
 fi
 
 # The script's directory. cutoff.call (the asterisk callfile) MUST be in the same dir as the script.
