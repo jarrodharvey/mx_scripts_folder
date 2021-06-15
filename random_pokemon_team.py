f7e6bd29956627d604563ff1e4ddc4f9d759bee8
jarrodharvey <jarrodharvey@gmail.com>
2021-06-15 19:36:23 +1000

    Pokemon update

--- random_pokemon_team.py
+++ random_pokemon_team.py
@@ -1,3 +1,3 @@
 import random
-a = ["Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "bait", "fakestall", "Trick-or-Treat!", "Frisk Scout"]
+a = ["Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "bait", "fakestall", "Trick-or-Treat!", "Frisk Tricker", "Disable Dusknoir"]
 print(random.choice(a))
