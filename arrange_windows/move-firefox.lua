d2b3e96ee08575d7ee6c12583a562b3d0b9dd79c
jarrodharvey <jarrodharvey@gmail.com>
2021-08-20 19:21:52 +1000

    Fixed window moving script.

--- arrange_windows/move-firefox.lua
+++ arrange_windows/move-firefox.lua
@@ -1,4 +1,4 @@
-if string.match(get_window_name(), "To Do") then
+if string.match(get_window_name(), "Sign in to your account") then
 	set_window_workspace(3);
 	set_window_geometry(1980, 20, 1920, 1050);
 	maximize();
