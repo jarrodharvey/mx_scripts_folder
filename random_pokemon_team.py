4c1e740203e23d40ea83aab1d76506c593d44aa1
jarrodharvey <jarrodharvey@gmail.com>
2022-06-27 20:25:25 +1000

    Update

--- random_pokemon_team.py
+++ random_pokemon_team.py
@@ -1,3 +1,3 @@
 import random
-a = ["Raboot", "Gravity", "Psychic Terrain", "Team Articuno", "Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "Fairy bait", "fakestall", "Trick-or-Treat!", "Frisk Tricker", "Scarf bait"]
+a = ["Magnet Raichu", "Gravity", "Psychic Terrain", "Team Articuno", "Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "Fairy bait", "fakestall", "Trick-or-Treat!", "Frisk Tricker", "Scarf bait"]
 print(random.choice(a))
