0c5c824ca02a672201af5fa35a27e327e3838022
jarrodharvey <jarrodharvey@gmail.com>
2022-06-29 21:55:18 +1000

    Pogeymans

--- random_pokemon_team.py
+++ random_pokemon_team.py
@@ -1,3 +1,3 @@
 import random
-a = ["Magnet Raichu", "Gravity", "Psychic Terrain", "Team Articuno", "Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "Fairy bait", "fakestall", "Trick-or-Treat!", "Frisk Tricker", "Scarf bait"]
+a = ["Belchy Slowking", "Magnet Raichu", "Gravity", "Psychic Terrain", "Team Articuno", "Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "Fairy bait", "fakestall", "Trick-or-Treat!", "Frisk Tricker", "Scarf bait"]
 print(random.choice(a))
