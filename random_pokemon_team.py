71065a5697e1a95d37469444ed188331a5947e19
jarrodharvey <jarrodharvey@gmail.com>
2022-07-02 13:50:43 +1000

    Removed the random pokemon team script

--- random_pokemon_team.py
+++
@@ -1,3 +0,0 @@
-import random
-a = ["Belchy Slowking", "Magnet Raichu", "Gravity", "Psychic Terrain", "Team Articuno", "Azu-stall", "Chubbs", "Umbreon", "Torkoal", "Goodra", "Comfey", "Pupper", "Gastrodon", "Wishi", "Fairy bait", "fakestall", "Trick-or-Treat!", "Frisk Tricker", "Scarf bait"]
-print(random.choice(a))
